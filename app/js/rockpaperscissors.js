0ca4aedc40625a8fc78de89ce5ca21117c97839c
citygent <cyberyt@gmail.com>
2015-07-29 14:52:42 +0100

    getwinner function playermove vs cpumove

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -21,7 +21,7 @@ function randomPlay() {
 /*           Write Your Code Below            */
 ////////////////////////////////////////////////
 
-function getPlayerMove(move) {
+function getplayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
@@ -35,12 +35,35 @@ function getComputerMove(move) {
     return move || randomPlay();
 }
 
-function getWinner(playerMove,computerMove) {
-    var winner;
+function getWinner(playerMove,computerMove) { //what given, what want?
+    var winner; // can be computer, player or tie.
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+if (playerMove === computerMove) {
+    winner = 'tie'
+}
+else {
+    if (playerMove == 'rock' && computerMove == 'scissors') {
+        winner = 'player';
+    }
+    else if (playerMove == 'rock' && computerMove == 'paper') {
+        winner = 'computer';
+    }
+    else if (playerMove == 'scissors' && computerMove == 'paper') {
+        winner = 'player';
+    }
+    else if (playerMove == 'scissors' && computerMove == 'rock') {
+        winner = 'computer';
+    }
+    else if (playerMove == 'paper' && computerMove == 'rock') {
+        winner = 'player';
+    }
+    else if (playerMove == 'paper' && computerMove == 'scissors') {
+        winner = 'computer';
+    }
+    else winner = 'something went wrong';
+}
     return winner;
 }
 
